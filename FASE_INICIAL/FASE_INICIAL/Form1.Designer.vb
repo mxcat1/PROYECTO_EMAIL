1827a4c5dfcd7da50da668d5da2b87aa19aff65f
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2017-09-24 20:20:20 -0500

    ggggg

---
+++ FASE_INICIAL/FASE_INICIAL/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnverificar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnverificar
+        '
+        Me.btnverificar.Location = New System.Drawing.Point(99, 215)
+        Me.btnverificar.Name = "btnverificar"
+        Me.btnverificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnverificar.TabIndex = 0
+        Me.btnverificar.Text = "VERIFICAR"
+        Me.btnverificar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnverificar)
+        Me.Name = "Form1"
+        Me.Text = "PRUEBA_1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnverificar As Button
+End Class
---
+++ FASE_INICIAL/FASE_INICIAL/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnverificar_Click(sender As Object, e As EventArgs) Handles btnverificar.Click
+        Dim fecha_actual As Date = Date.Today
+        Dim dia_actual, dia_de_la_semana(6) As String
+        dia_de_la_semana(0) = "DOMINGO"
+        dia_de_la_semana(1) = "LUNES"
+        dia_de_la_semana(2) = "MARTES"
+        dia_de_la_semana(3) = "MIERCOLES"
+        dia_de_la_semana(4) = "JUEVES"
+        dia_de_la_semana(5) = "VIERNES"
+        dia_de_la_semana(6) = "SABADO"
+        dia_actual = fecha_actual.DayOfWeek
+
+        MsgBox("LA FECHA ACTUAL ES " + fecha_actual + " EL DIA ACTUAL ES: " + dia_de_la_semana(dia_actual))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
