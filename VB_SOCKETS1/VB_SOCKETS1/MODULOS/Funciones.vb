f9a93b6aa20984f439ed12a51fdecb118c3549a6
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2017-10-18 22:36:04 -0500

    CAMBIOS NO ENTIENDO MUY BIEN LO DE SOCKETS (PENDIENTE)

--- VB_SOCKETS1/VB_SOCKETS1/MODULOS/Funciones.vb
+++ VB_SOCKETS1/VB_SOCKETS1/MODULOS/Funciones.vb
@@ -3,6 +3,7 @@ Imports System.Net
 Imports System.Net.Sockets
 Imports System.Threading
 Module Funciones
+
     'SERVER CONFIGURACION
     Public servidorIP As IPAddress = IPAddress.Parse("127.0.0.1")
     Public servidorpuerto As Integer = 4444
@@ -14,6 +15,7 @@ Module Funciones
 
     Public cliente As TcpClient
     Public cliente_datos As StreamReader
+    Public cliente_envia As StreamWriter
 
     Public Sub inicio()
         servidor = New TcpListener(servidorIP, servidorpuerto)
@@ -24,12 +26,29 @@ Module Funciones
 
     End Sub
     Function escuchar()
-
+        Dim mensaje As String = ""
         Do Until escucha = False
+            If servidor.Pending Then
+                cliente = servidor.AcceptTcpClient
+                cliente_datos = New StreamReader(cliente.GetStream)
+                While cliente_datos.Peek > -1
+
+                    mensaje &= cliente_datos.Read().ToString
+                End While
+                Return mensaje
+            End If
 
         Loop
 
+        Thread.Sleep(100)
 
     End Function
+
+    Public Sub enviar(datos As String)
+        cliente = New TcpClient(servidorIP.ToString, servidorpuerto)
+        cliente_envia = New StreamWriter(cliente.GetStream)
+        cliente_envia.Write(datos)
+
+    End Sub
 
 End Module
